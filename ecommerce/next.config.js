b0162d00e8caab9f4d3157d2eee9f48df01c293d
igorovic <hello@dyve.dev>
2022-02-18 09:29:35 +0100

    update nextjs compiler settings

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -1,10 +1,16 @@
-const { i18n } = require('./next-i18next.config');
+const { i18n } = require("./next-i18next.config");
 
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n,
   reactStrictMode: true,
   images: {
-    domains: ['cdn.chec.io'],
+    domains: ["cdn.chec.io"],
   },
-}
+  compiler: {
+    reactRemoveProperties: true,
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+};
