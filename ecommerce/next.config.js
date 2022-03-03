9131e3f659f3f12e4b1b9e6b5e0f42f2d4211b8a
igorovic <hello@dyve.dev>
2022-03-03 09:20:34 +0100

    wip

--- ecommerce/next.config.js
+++ ecommerce/next.config.js
@@ -1,16 +1,30 @@
 const { i18n } = require("./next-i18next.config");
 
+const isDev = String(process.env.NODE_ENV).toLowerCase().startsWith("dev");
+console.log("next.config development:", isDev, "env:", process.env.NODE_ENV);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+let config = {
   i18n,
   reactStrictMode: true,
   images: {
     domains: ["cdn.chec.io"],
   },
   compiler: {
-    reactRemoveProperties: true,
-    removeConsole: {
-      exclude: ["error"],
-    },
+    reactRemoveProperties: !isDev,
   },
 };
+
+if (!isDev) {
+  config = {
+    ...config,
+    compiler: {
+      ...config.compiler,
+      removeConsole: {
+        exclude: ["error"],
+      },
+    },
+  };
+}
+
+module.exports = config;
