4d4302a62aed0cc9c5cedbeae93a6f690fd99df2
Igor Petrovic <igor@dyve.ch>
2022-07-04 12:51:41 +0200

    fix after refact

--- base/pages/_app.tsx
+++ base/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
-import store from "app/store";
+import store from "lib/app/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
